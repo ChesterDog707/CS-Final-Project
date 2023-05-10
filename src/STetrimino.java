5ea29d566c124ea1f8839a63d014ad0b563a5418
mccurdym <mccurdym@HS-B205-21441.GLASTONBURYUS.ORG>
2023-05-10 12:38:14 -0400

    S almiost done

--- src/STetrimino.java
+++ src/STetrimino.java
@@ -11,12 +11,72 @@ public class STetrimino extends Tetrimino {
 	@Override
 	public void rotate() {
 		// TODO Auto-generated method stub
-		
+		placeOrDelete(false);
+		switch(orientation) {
+		case 1:
+			if(checkPlacement(xPosition + 1, yPosition - 1, 2)) {
+				orientation = 2;
+				xPosition ++;
+				yPosition --;
+			}
+			break;
+		case 2:
+			if(checkPlacement(xPosition - 1, yPosition + 2, 3)) {
+				orientation = 3;
+				xPosition --;
+				yPosition +=2;
+			}
+			break;
+		case 3:
+			if(checkPlacement(xPosition, yPosition - 2, 4)) {
+				orientation = 4;
+				yPosition -=2;
+			}
+			break;
+		case 4:
+			if(checkPlacement(xPosition, yPosition + 1, 1)) {
+				orientation = 1;
+				yPosition += 1;
+			}
+			break;
+		}
+		placeOrDelete(true);
+		return;
 	}
 
 	@Override
 	public boolean placeOrDelete(boolean place) {
 		// TODO Auto-generated method stub
+		int num;
+		if(place)
+			num = numberValue;
+		else 
+			num=0;
+		if(checkPlacement(xPosition, yPosition, orientation)) {
+			switch(orientation) {
+			
+			case 3:
+			case 1:  //filling in the blocks on the 2d array with the "num"; making them into a different color
+				board[xPosition][yPosition] = num;  
+				board[xPosition+1][yPosition] = num;
+				board[xPosition+1][yPosition-1] = num;
+				board[xPosition+2][yPosition-1] = num;
+				return true;
+	
+			case 4:
+			case 2:
+				board[xPosition][yPosition] = num;
+				board[xPosition][yPosition+1] = num;
+				board[xPosition+1][yPosition+1] = num;
+				board[xPosition+1][yPosition+2] = num;
+			}
+		}
+		
+		
+		
+		
+		
+		
 		return false;
 	}
 	@Override
@@ -30,6 +90,14 @@ public class STetrimino extends Tetrimino {
 	@Override
 	protected boolean checkPlacement(int changeX, int changeY, int changeOrientation) {
 		// TODO Auto-generated method stub
+		switch(orientation) {
+		case 1:
+		
+		
+		
+		
+		
+		
 		return false;
 	}
 
