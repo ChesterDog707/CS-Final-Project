996b7a80b84977445cb5c64e9be024bd54195a00
ChesterDog707 <60673795+ChesterDog707@users.noreply.github.com>
2023-05-30 16:56:08 -0400

    Fixed soft drop continuing between restarts

--- src/Panel.java
+++ src/Panel.java
@@ -1,9 +1,7 @@
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -225,6 +223,7 @@ public class Panel extends JPanel{
 				repaint();
 			else {
 				gameState = 3;
+				tick = tickAtLevel[0];
 				repaint();
 			}
 		}
